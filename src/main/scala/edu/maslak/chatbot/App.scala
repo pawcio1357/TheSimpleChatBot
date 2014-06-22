9ed08904286a54483d219e890ffa4b2226e30b4c
Pawel Maslak <maslak.pawel.1357@gmail.com>
2014-06-22 20:48:50 +0200

    fixed jar packaging with maven

--- src/main/scala/edu/maslak/chatbot/App.scala
+++ src/main/scala/edu/maslak/chatbot/App.scala
@@ -14,28 +14,26 @@ object App {
   def main(args: Array[String]) {
 
     println(BANNER);
-    
-   
-    var importName:String = null
-    var exportName:String = null
-    var escapeWord:String = null
-    var learnName:String = null
-    
+
+    var importName: String = null
+    var exportName: String = null
+    var escapeWord: String = null
+    var learnName: String = null
 
     // parse the command line arguments
     // e.g. chatbot --escape exit --input in.txt --output out.txt
-    args reduce ((arg1:String, arg2:String) => {
-      arg1 match {
-        case "--input"|"-i" => importName = arg2
-        case "--output"|"-o" => exportName = arg2
-        case "--escape"|"-e" => escapeWord = arg2
-        case "--learn"|"-l" => learnName = arg2
-        case _ =>
-      }
-      arg2
-    })
-    
-    
+    if (args.size > 0)
+      args reduce ((arg1: String, arg2: String) => {
+        arg1 match {
+          case "--input" | "-i" => importName = arg2
+          case "--output" | "-o" => exportName = arg2
+          case "--escape" | "-e" => escapeWord = arg2
+          case "--learn" | "-l" => learnName = arg2
+          case _ =>
+        }
+        arg2
+      })
+
     val bot = escapeWord match {
       case null => new ChatBot
       case word => new ChatBot(word)
@@ -45,7 +43,6 @@ object App {
     if (importName != null)
       println(bot load importName)
 
-      
     // main loop
     while (bot running) {
       print(PROMPT)
